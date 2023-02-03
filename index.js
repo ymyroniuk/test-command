294989ae4fb58e070338c30d8228c19bd1c300a7
Yarik <yarikrst@gmail.com>
2023-02-03 18:50:47 +0200

    changes about file js

---
+++ index.js
@@ -0,0 +1,15 @@
+const obj = [{
+    apple: 'apple',
+    orange: 'orange',
+    black: 'black',
+    fruits: {
+        pineapple: 'pineapple',
+        limon: 'limon',
+        banana: 'banana'
+    }
+}]
+
+console.log(obj);
+
+const a = obj[0]
+console.log(a);
